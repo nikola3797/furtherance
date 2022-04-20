b887f9e0e82db1bbc0d03770f232b6a6015a5458
nikola <nikola.zeka97@gmail.com>
2022-04-20 15:11:02 +0200

    text changes

--- components/PeopleBehind/index.js
+++ components/PeopleBehind/index.js
@@ -8,22 +8,30 @@ import thirdPic from '../../assets/images/3.png';
 const PeopleBehindUs = () => {
     return (
         <div className={styles.peopleBehindUs}>
-            <h1>Who are the people behind the furtherance</h1>
-            <p className={styles.peopleBehindUsP}>Furtherance has united them, in order to provide a better and more qualified network to help startups and scaleups reach their goals faster
-                and expand their business. Want to qualify for our network? Reach us here: <span className={styles.mail} onClick={() => window.location = 'mailto:network@furtherance.rs'}>network@furtherance.rs</span>
+            <h1>Who are the people behind Furtherance</h1>
+            <p className={styles.peopleBehindUsP}>Furtherance is a private matchmaking platform that helps startup founders like yourself find the right fit for your current challenges. The other side of the platform consist of a private network of both industry experts and outsource agencies for all sorts of topics like sales, HR, marketing, Near-shoring, fundraising etc. Furtherance has created and verified that network in order to provide startups and scaleups with a single platform through which they can find the right partner to help them go further in their business
+                Want to qualify for our network? Reach us at: <span className={styles.mail} onClick={() => window.location = 'mailto:sales@furtherance.rs'}>sales@furtherance.rs</span>
             </p>
             <div className={styles.pictureRow}>
-                <div>
+                <div style={{width: '33%'}}>
+                    <h2 style={{marginTop: '0px'}}>Who</h2>
                     <Image src={firstPic} />
-                    <p className={styles.peopleBehindUsSubP}>We have been connecting startups and scaleups in their ecosystems for the past 10 years</p>
+                    <p className={styles.peopleBehindUsSubP}>Furtherance is a collective of founders, sales and marketing experts, fundraising consultants and outsource agencies for all relevant topics that startups need.</p>
                 </div>
-                <div>
+                <div style={{width: '33%'}}>
+                    <h2 style={{marginTop: '0px'}}>What</h2>
                     <Image src={secondPic} />
-                    <p className={styles.peopleBehindUsSubP}>Furtherance is a collective of founders, sales experts, marketing experts, and VC investors.</p>
+                    <p className={styles.peopleBehindUsSubP}>Services include:<br/>
+                        Consultants in the field of fundraising, sales, marketing, business development and 
+                        Outsource agencies for<br/> HR and hiring nearshore/offshore IT teams or Sales teams for UK/USA/AU/Balkans</p>
                 </div>
-                <div>
+                <div style={{width: '33%', marginBottom: '20px'}}>
+                    <h2 style={{marginTop: '0px'}}>Why</h2>
                     <Image src={thirdPic} />
-                    <p className={styles.peopleBehindUsSubP}>Their reasons for doing so were simple, they knew people, and they liked helping founders in their ecosystem.</p>
+                    <p className={styles.peopleBehindUsSubP}>
+                        Furtherance was formed because the founding members simply knew a lot of people, and liked connecting and helping founders
+                        in their ecosystem. They understood how important one person can be to the success or failure of a
+                        small and growing startup, so they created Furtherance.</p>
                 </div>
             </div>
         </div>
