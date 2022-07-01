673cc16a7d972670e98df37204e7fe047a79adba
nikola <nikola.zeka97@gmail.com>
2022-07-01 12:02:56 +0200

    added calendar link to book me button

--- components/BookAMeetingBtn/index.js
+++ components/BookAMeetingBtn/index.js
@@ -2,11 +2,14 @@ import styles from '../../styles/Home.module.css';
 
 const BookAMeetingBtn = () =>{
     return (
-        <button
+        <a
             className={styles.bookAMetting}
+            href='https://calendly.com/vladislav-pavlov1/30min'
+            target="_blank"
+            rel="noopener noreferrer"
         >
             Book a meeting
-        </button>
+        </a>
     );
 };
 
